47853cb10b1405ee9d117c60fad71ae4df4a36f0
briangearty <geartybrian@gmail.com>
2021-11-11 20:52:17 -0500

    change

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,8 @@ function authIg(){
 	let url = `https://api.instagram.com/oauth/authorize?client_id=${appId}&redirect_uri=${redUri}&scope=user_profile,user_media&response_type=code`;
 	window.open(url, "_blank").focus();
 
+    console.log(redUri)
+
     // fetch(url)
     // .then((response) => response.json())
     // .then((data) => console.log(data))
