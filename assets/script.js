e9cf2c3ffb24a5fe2c415ab6cb001e97f6f133ec
briangearty <geartybrian@gmail.com>
2021-11-11 21:47:16 -0500

    changing redUri

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ igConnectBtn.addEventListener("click", function(){
 
 function authIg(){
     let appId = "215321604061729";
-	let redUri = window.location.origin;
+	let redUri = window.location.origin + "/igApp";
 	let url = `https://api.instagram.com/oauth/authorize?client_id=${appId}&redirect_uri=${redUri}&scope=user_profile,user_media&response_type=code`;
 	window.open(url, "_blank").focus();
 
