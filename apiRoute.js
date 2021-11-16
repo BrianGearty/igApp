4425bd79545529573af9297802c4919cd55c571a
briangearty <geartybrian@gmail.com>
2021-11-16 00:47:26 -0500

    cmon man more stupid

--- apiRoute.js
+++ apiRoute.js
@@ -50,7 +50,7 @@ async function getUser(userInfo){
     console.log("HIT GET USER WITH TOKEN", userInfo)
 
     try{
-    const response = await fetch(`https://graph.instagram.com/v12.0/${userInfo.user_id}?fields=id,username&access_token=${userInfo.accessToken}`)
+    const response = await fetch(`https://graph.instagram.com/v12.0/${userInfo.userId}?fields=id,username&access_token=${userInfo.accessToken}`)
     const data = await response.json();
     console.log("USERR", data)
 
