c4d7745a413bf860d94c18a7db5f28cd7a0b670e
briangearty <geartybrian@gmail.com>
2021-11-15 22:16:43 -0500

    moving

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const path = require("path")
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
