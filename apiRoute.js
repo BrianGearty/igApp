affd0306364fb492c912511927c21a259dc21409
briangearty <geartybrian@gmail.com>
2021-11-15 22:22:44 -0500

    post

--- apiRoute.js
+++ apiRoute.js
@@ -4,6 +4,7 @@ const request = require('request');
 let code;
 let redirect_uri;
 
+console.log("code outside")
 
 // let form = {
 //     client_id: process.env.CLIENT_ID,
@@ -15,39 +16,29 @@ let redirect_uri;
 
 router.post('/api/insta', async (req, res) => {
         
-    let form = {
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        grant_type: 'authorization_code',
-        redirect_uri: req.body.redirect_uri,
-        code: req.body.code
-    }
-
-    console.log(form)
-
     let accessToken = null;
     
         console.log("WE BE GETTING PLACES")
 
-        // send form based request to Instagram API
-        // const result = await request.post({
-        //     url: 'https://api.instagram.com/oauth/access_token',
-        //     form: {
-        //         client_id: process.env.CLIENT_ID,
-        //         client_secret: process.env.CLIENT_SECRET,
-        //         grant_type: 'authorization_code',
-        //         redirect_uri: req.body.redirect_uri,
-        //         code: req.body.code
-        //     }
+        //send form based request to Instagram API
+        const result = await request.post({
+            url: 'https://api.instagram.com/oauth/access_token',
+            form: {
+                client_id: process.env.CLIENT_ID,
+                client_secret: process.env.CLIENT_SECRET,
+                grant_type: 'authorization_code',
+                redirect_uri: req.body.redirect_uri,
+                code: req.body.code
+            }
 
-        // });
+        });
 
-        // console.log("RESPONSE FROM INSTAGRAM RESULT", result)
-        // accessToken = JSON.parse(result).access_token;
-        // //res.send(accessToken)
+        console.log("RESPONSE FROM INSTAGRAM RESULT", result)
+        accessToken = res.json(result)
+        //res.send(accessToken)
 
-        // console.log("ERROR HAPPENS HERE", e);
-        // res.status(500).json(e.response.data)
+        console.log("ERROR HAPPENS HERE", e);
+        res.status(500).json(e.response.data)
 })
 
 
