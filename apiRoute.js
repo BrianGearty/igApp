5135eef69a5d7036e9eedef70d3fe05b07ef9001
briangearty <geartybrian@gmail.com>
2021-11-16 14:44:11 -0500

    errors on data from user

--- apiRoute.js
+++ apiRoute.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const fetch = require("node-fetch")
 
-
+// getting auth code from front end
 router.post('/api/insta', async (req, res) => {
-
+    // passing our auth code and putting it in x-ww-form-urlencoded format
     let form = `client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=${req.body.redirect_uri}&code=${req.body.code}`
-    
+    // node-fetch hitting instagrams access token end point to exchange our auth code for access token
     try{
         const response = await fetch('https://api.instagram.com/oauth/access_token' ,{
             method: "POST",
@@ -14,14 +14,16 @@ router.post('/api/insta', async (req, res) => {
             },
             body: form,
         })
+        // response from instagram
         const data = await response.json();
 
-
+        // parase data into object
         let userInfo = {
             accessToken: data.access_token,
             userId: data.user_id
         }
         console.log("ACCCCCESSSSS TOKENNNNNN",userInfo)
+        // pass parsed data into getUser function
         getUser(userInfo)
     } catch (err){
 
@@ -30,13 +32,14 @@ router.post('/api/insta', async (req, res) => {
     }
 })
 
+// here we call instagrams end point to get user info
 async function getUser(userInfo){
     console.log("HIT GET USER WITH TOKEN", userInfo)
 
     try{
     const response = await fetch(`https://graph.instagram.com/v12.0/${userInfo.userId}/media?access_token=${userInfo.accessToken}`)
     const data = await response.json();
-    console.log("MEDIA FROM USER", JSON.stringify(data.media.data))
+    console.log("MEDIA FROM USER", data)
 
     let user = {
         id: data.id,
