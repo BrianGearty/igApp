dac935ce1ac3f196ff5ce048c2487550054b6f16
briangearty <geartybrian@gmail.com>
2021-11-16 21:57:06 -0500

    style.css

--- public/assets/script.js
+++ public/assets/script.js
@@ -63,7 +63,7 @@ getPhotos = () => {
         for(var i = 0; i < data[0].length; i++){
 
             let card = document.createElement("div")
-            card.setAttribute("class", "card mb-5")
+            card.setAttribute("class", "card")
             let cardBody = document.createElement("div")
             cardBody.setAttribute("class", "card-body")
 
