bf3e8eaebb6097f39fa017774e12628feb724b00
briangearty <geartybrian@gmail.com>
2021-11-27 17:46:57 -0500

    creating carousel w template literals

--- public/assets/script.js
+++ public/assets/script.js
@@ -24,7 +24,7 @@ authIgBtn.addEventListener("click", function () {
 
 })
 
-cleartokenBtn.addEventListener("click", function(){
+cleartokenBtn.addEventListener("click", function () {
     window.location.href = "https://stark-chamber-84959.herokuapp.com/"
 })
 
@@ -102,67 +102,90 @@ getPhotos = () => {
                     //"https://scontent-iad3-2.cdninstagram.com/v/t51.2885-15/39926281_2134387383513684_1025651470323679232_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=YXcS5lKub7oAX9PifaM&_nc_ht=scontent-iad3-2.cdninstagram.com&edm=ANo9K5cEAAAA&oh=894a1feeea4720d9185c9a4909963150&oe=6199CE09"
 
                 } else if (instaStuff.media_type === 'CAROUSEL_ALBUM') {
-                    let carouselItem = document.createElement("div")
-                    carouselItem.setAttribute("class", "carousel-item")
 
-                    let carouselImg = document.createElement("img")
+                    console.log(instaStuff.children.data)
+                    // let carouselItem = document.createElement("div")
+                    // carouselItem.setAttribute("class", "carousel-item")
+
+                    // let carouselImg = document.createElement("img")
                     // console.log("INSTA STUFF.CHILDREN.DATA",instaStuff.children.data.length)
 
-                    instaStuff.children.data.forEach(function(img, i){
-                        console.log("CHILDREN DATA", img)
-                        console.log("img index", i)
-                        
-
-                        carouselImg.setAttribute("class", "d-block w-100 card-image-top")
-                        carouselImg.setAttribute("src", img.media_url)
-                        carouselImg.setAttribute("alt", "carousel picture")
-
-                        
-                        carouselItem.append(carouselImg)
-
-                        //console.log("CAROUSEL Item", carouselItem)
-                    })
-                    
-
-                    let carousel = document.createElement("div")
-                    carousel.setAttribute("class", "carousel slide")
-                    carousel.setAttribute("data-ride", "carousel")
-                    carousel.setAttribute("id", "carouselControls")
-
-                    let carouselInner = document.createElement("div")
-                    carouselInner.setAttribute("class", "carousel-inner")
-
-                    let prevTag = document.createElement("a")
-                    let nextTag = document.createElement("a")
-                    prevTag.setAttribute("class", "carousel-control-prev")
-                    prevTag.setAttribute("href", "#carouselControls")
-                    prevTag.setAttribute("role", "button")
-                    prevTag.setAttribute("data-slide", "prev")
-                    nextTag.setAttribute("class", "carousel-control-next")
-                    nextTag.setAttribute("href", "#carouselControls")
-                    nextTag.setAttribute("role", "button")
-                    nextTag.setAttribute("data-slide", "next")
-
-                    let prevSpan = document.createElement("span")
-                    let nextSpan = document.createElement("span")
-                    prevSpan.setAttribute("class", "carousel-control-prev-icon")
-                    prevSpan.setAttribute("aria-hidden", "false")
-                    nextSpan.setAttribute("class", "carousel-control-next-icon")
-                    nextSpan.setAttribute("aria-hidden", "false")
-
-                    prevTag.append(prevSpan)
-                    nextTag.append(nextSpan)
+                    // instaStuff.children.data.forEach(function(img, i){
+                    //     console.log("CHILDREN DATA", img)
+                    //     console.log("img index", i)
 
 
+                    //     carouselImg.setAttribute("class", "d-block w-100 card-image-top")
+                    //     carouselImg.setAttribute("src", img.media_url)
+                    //     carouselImg.setAttribute("alt", "carousel picture")
+
+
+                    //     carouselItem.append(carouselImg)
+
+                    //console.log("CAROUSEL Item", carouselItem)
+                    // })
+
+
+                    // let carousel = document.createElement("div")
+                    // carousel.setAttribute("class", "carousel slide")
+                    // carousel.setAttribute("data-ride", "carousel")
+                    // carousel.setAttribute("id", "carouselControls")
+
+                    // let carouselInner = document.createElement("div")
+                    // carouselInner.setAttribute("class", "carousel-inner")
+
+                    // let prevTag = document.createElement("a")
+                    // let nextTag = document.createElement("a")
+                    // prevTag.setAttribute("class", "carousel-control-prev")
+                    // prevTag.setAttribute("href", "#carouselControls")
+                    // prevTag.setAttribute("role", "button")
+                    // prevTag.setAttribute("data-slide", "prev")
+                    // nextTag.setAttribute("class", "carousel-control-next")
+                    // nextTag.setAttribute("href", "#carouselControls")
+                    // nextTag.setAttribute("role", "button")
+                    // nextTag.setAttribute("data-slide", "next")
+
+                    // let prevSpan = document.createElement("span")
+                    // let nextSpan = document.createElement("span")
+                    // prevSpan.setAttribute("class", "carousel-control-prev-icon")
+                    // prevSpan.setAttribute("aria-hidden", "false")
+                    // nextSpan.setAttribute("class", "carousel-control-next-icon")
+                    // nextSpan.setAttribute("aria-hidden", "false")
+
+                    // prevTag.append(prevSpan)
+                    // nextTag.append(nextSpan)
+
+                    let carousel = `
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="..." alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="..." alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="..." alt="Third slide">
+                        </div>
+                    </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>`
 
 
 
 
 
-                    carouselInner.append(carouselItem)
-                    carousel.append(carouselInner, prevTag, nextTag)
-                    cardBody.append(carousel)
-                    console.log(carousel)
+                    // carouselInner.append(carouselItem)
+                    // carousel.append(carouselInner, prevTag, nextTag)
+                    // cardBody.append(carousel)
+                    // console.log(carousel)
                 } else {
 
                 }
@@ -186,6 +209,6 @@ getPhotos = () => {
 
 }
 
-setTimeout(()=>{
- getPhotos();
+setTimeout(() => {
+    getPhotos();
 }, 2000)
